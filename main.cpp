7c5623db7dc345a4e21259ce080e8d6d5659be4e
Hikashi <joannakasprzak@users.noreply.github.com>
2016-03-16 20:52:55 +0100

    Implemenatcja programu

--- main.cpp
+++ main.cpp
@@ -1,6 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <iostream>
+
+using namespace std;
 
 char **charSquare(int n);
 void drawCharSquare(char **square, int n);
@@ -8,16 +11,38 @@ void drawCharSquare(char **square, int n);
 int main(int argc, char **argv)
 {
 	int n;
+		
+	n = atoi(argv[1]);
+	srand(atoi(argv[2]));
+
 	drawCharSquare(charSquare(n), n);	
 }
 
 char **charSquare(int n)
 {
 	char **square;
+
+	square = new char*[n];
+
+	int wymiar = 3; 
+ 
+	for (int i = 0; i<n; i++){
+		square[i] = new char[n];
+		for (int j = 0; j<n; j++){
+			int losowa = 97 + rand() % 25;
+			square[i][j] = (char) losowa;
+		}
+	}
+	
 	return square;
 }
 
 void drawCharSquare(char **square, int n)
 {
-
+	for (int i = 0; i<n; i++){
+		for (int j = 0; j<n; j++){
+			cout << square[i][j] << " ";
+		}
+		cout << endl;
+	}
 }
